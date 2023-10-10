3897c928acd2a50bac4e53ba3f2e0ee3bcf853a2
Elle <128157075+elle-concetta@users.noreply.github.com>
2023-10-09 23:23:44 -0400

    Update test_BookRatings.py

--- tests/test_BookRatings.py
+++ tests/test_BookRatings.py
@@ -2,15 +2,20 @@
 import json
 from flask_testing import TestCase
 import unittest
-from BookRatings_api import app  # 'BookRatings_api.py' REST API code
-
+from unittest.mock import patch, MagicMock
+from BookRatings_api import app
 
 class TestApp(TestCase):
     def create_app(self):
         app.config['TESTING'] = True
         return app
 
-    def test_create_rating(self):
+    @patch('your_module.mysql.connector.connect')
+    def test_create_rating(self, mock_connect):
+        # Mocking the database connection
+        mock_cursor = MagicMock()
+        mock_connect.return_value.cursor.return_value = mock_cursor
+
         # Your payload
         payload = {
             "book_id": 1,
@@ -30,6 +35,8 @@ class TestApp(TestCase):
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(data["message"], "Rating created successfully")
 
+        # Additional assertions on database interactions can be done here if needed
+        mock_cursor.execute.assert_called_once()
 
 # This allows us to run our tests using the typical `python -m unittest discover` command
 if __name__ == '__main__':
