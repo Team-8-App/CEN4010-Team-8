bae2b3a10eb1e46d9c91a6709b66ae316249f88d
Elle <128157075+elle-concetta@users.noreply.github.com>
2023-10-22 23:49:09 -0400

    Update book_api.py
    Added POST and PUSH

--- services/book_browsing_and_sorting/book_api.py
+++ services/book_browsing_and_sorting/book_api.py
@@ -11,6 +11,10 @@ db_config = {
     'database': 'team_8'
 }
 
+headers = {
+    'Content-Type': 'application/json'
+}
+# GET endpoint to retrieve books
 @app.route('/books', methods=['GET'])
 def get_books():
     genre_filter = request.args.get('genre')
@@ -21,7 +25,7 @@ def get_books():
 
     valid_sort_columns = ["title", "genre", "copies_sold", "rating", "publisher", "discount"]
     if sort_by not in valid_sort_columns:
-        return jsonify({'error': 'Invalid sort column'})
+        return jsonify({'error': 'Invalid sort column'}), 400
 
     conn = None
     cursor = None
@@ -56,7 +60,7 @@ def get_books():
         return jsonify(books)
 
     except mysql.connector.Error as err:
-        return jsonify({'error': 'Database error', 'message': str(err)})
+        return jsonify({'error': 'Database error', 'message': str(err)}), 500
 
     finally:
         if cursor:
@@ -64,6 +68,101 @@ def get_books():
         if conn and conn.is_connected():
             conn.close()
 
+# POST endpoint to create a new book
+@app.route('/books', methods=['POST'])
+def create_book():
+    data = request.json
+
+    title = data.get('title')
+    genre = data.get('genre')
+    publisher = data.get('publisher')
+    rating = data.get('rating')
+    copies_sold = data.get('copies_sold', 0)
+    discount = data.get('discount', 0)
+
+    if not title or not genre or not publisher or rating is None:
+        return jsonify({'error': 'Missing required fields'}), 400
+
+    conn = None
+    cursor = None
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+
+        query = "INSERT INTO Books (title, genre, publisher, rating, copies_sold, discount) VALUES (%s, %s, %s, %s, %s, %s)"
+        cursor.execute(query, (title, genre, publisher, rating, copies_sold, discount))
+        conn.commit()
+
+        return jsonify({'message': 'Book created successfully'}), 201
+
+    except mysql.connector.Error as err:
+        return jsonify({'error': 'Database error', 'message': str(err)}), 500
+
+    finally:
+        if cursor:
+            cursor.close()
+        if conn and conn.is_connected():
+            conn.close()
+
+# PUT endpoint to update a book
+@app.route('/books/<int:book_id>', methods=['PUT'])
+def update_book(book_id):
+    data = request.json
+
+    title = data.get('title')
+    genre = data.get('genre')
+    publisher = data.get('publisher')
+    rating = data.get('rating')
+    copies_sold = data.get('copies_sold')
+    discount = data.get('discount')
+
+    if not title and not genre and not publisher and rating is None and copies_sold is None and discount is None:
+        return jsonify({'error': 'No fields to update'}), 400
+
+    conn = None
+    cursor = None
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+
+        query = "UPDATE Books SET"
+        query_args = []
+        if title:
+            query += " title=%s,"
+            query_args.append(title)
+        if genre:
+            query += " genre=%s,"
+            query_args.append(genre)
+        if publisher:
+            query += " publisher=%s,"
+            query_args.append(publisher)
+        if rating is not None:
+            query += " rating=%s,"
+            query_args.append(rating)
+        if copies_sold is not None:
+            query += " copies_sold=%s,"
+            query_args.append(copies_sold)
+        if discount is not None:
+            query += " discount=%s,"
+            query_args.append(discount)
+
+        query = query.rstrip(',')
+        query += " WHERE id=%s"
+        query_args.append(book_id)
+
+        cursor.execute(query, query_args)
+        conn.commit()
+
+        return jsonify({'message': 'Book updated successfully'}), 200
+
+    except mysql.connector.Error as err:
+        return jsonify({'error': 'Database error', 'message': str(err)}), 500
+
+    finally:
+        if cursor:
+            cursor.close()
+        if conn and conn.is_connected():
+            conn.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
