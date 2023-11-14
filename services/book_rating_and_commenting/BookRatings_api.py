780fd0ed84944c767492f09044305e591c8998d1
Jayleen Espinal <jayleen.espinal@gmail.com>
2023-11-13 22:18:33 -0500

    Recreated BookRatings_api
    Now it works, the test.py is having some trouble but the api actually works properly

--- services/book_rating_and_commenting/BookRatings_api.py
+++ services/book_rating_and_commenting/BookRatings_api.py
@@ -5,76 +5,122 @@ app = Flask(__name__)
 
 # Database configuration
 db_config = {
-    'host': 'HOST_8',
+    'host': 'ls-b2f10f0f8d1f46949bc16b2a5608934e887eb6b0.c1zf3hrzxwhy.us-east-2.rds.amazonaws.com',
     'user': 'team_8',
-    'password': 'PASSWORD_8',
+    'password': 'Ankit-Bonnie-082821!',
     'database': 'team_8',
-    'raise_on_warnings': True  # Added a comma before this line
+    'raise_on_warnings': True
 }
 
 
-@app.route('/ratings', methods=['POST'])
-def create_rating():
+@app.route('/ratings', methods=['GET', 'POST'])
+def ratings():
     conn = None
     cursor = None
 
-    try:
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
+    if request.method == 'GET':
+        try:
+            conn = mysql.connector.connect(**db_config)
+            cursor = conn.cursor(dictionary=True)
+            query = "SELECT * FROM Ratings"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return jsonify(result)
 
-        book_id = request.json['book_id']
-        user_id = request.json['user_id']
-        rating = request.json['rating']
+        except mysql.connector.Error as err:
+            return jsonify({'error': 'Database error', 'message': str(err)})
 
-        insert_query = ("INSERT INTO BookRatings (book_id, user_id, rating) "
-                        "VALUES (%s, %s, %s)")
-        cursor.execute(insert_query, (book_id, user_id, rating))
-        conn.commit()
+        finally:
+            if cursor:
+                cursor.close()
+            if conn and conn.is_connected():
+                conn.close()
 
-        return jsonify({'message': 'Rating created successfully'}), 201
+    elif request.method == 'POST':
+        try:
+            conn = mysql.connector.connect(**db_config)
+            cursor = conn.cursor()
 
-    except mysql.connector.Error as err:
-        return jsonify({'error': 'Database error', 'message': str(err)}), 500
+            data = request.json
+            book_id, user_id, rating = data.get('book_id'), data.get('user_id'), data.get('rating')
 
-    finally:
-        if cursor:
-            cursor.close()
-        if conn and conn.is_connected():
-            conn.close()
+            if not all([book_id, user_id, rating]):
+                return jsonify({'error': 'Missing required fields'}), 400
+
+            query = "INSERT INTO Ratings (book_id, user_id, rating) VALUES (%s, %s, %s)"
+            params = (book_id, user_id, rating)
+
+            cursor.execute(query, params)
+            conn.commit()
+
+            return jsonify({'message': 'Rating created successfully'}), 201
+
+        except mysql.connector.Error as err:
+            return jsonify({'error': 'Database error', 'message': str(err)})
+
+        finally:
+            if cursor:
+                cursor.close()
+            if conn and conn.is_connected():
+                conn.close()
 
 
-@app.route('/ratings/comment', methods=['POST'])
-def create_comment():
+@app.route('/comments', methods=['GET', 'POST'])
+def comments():
     conn = None
     cursor = None
 
-    try:
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
+    if request.method == 'GET':
+        # Handle GET request for comments
+        try:
+            conn = mysql.connector.connect(**db_config)
+            cursor = conn.cursor(dictionary=True)
 
-        book_id = request.json['book_id']
-        user_id = request.json['user_id']
-        comment = request.json['comment']
+            query = "SELECT * FROM Comments"
+            cursor.execute(query)
+            comments = cursor.fetchall()
 
-        insert_query = ("INSERT INTO BookRatings (book_id, user_id, comment, rating) "
-                        "VALUES (%s, %s, %s, %s)")
-        cursor.execute(insert_query, (book_id, user_id, comment))
-        conn.commit()
+            return jsonify(comments)
 
-        return jsonify({'message': 'Comment created successfully'}), 201
+        except mysql.connector.Error as err:
+            return jsonify({'error': 'Database error', 'message': str(err)})
 
-    except mysql.connector.Error as err:
-        return jsonify({'error': 'Database error', 'message': str(err)}), 500
+        finally:
+            if cursor:
+                cursor.close()
+            if conn and conn.is_connected():
+                conn.close()
 
-    finally:
-        if cursor:
-            cursor.close()
-        if conn and conn.is_connected():
-            conn.close()
+    elif request.method == 'POST':
+        # Handle POST request for comments
+        data = request.json
+        book_id, user_id, comment = data.get('book_id'), data.get('user_id'), data.get('comment')
+
+        if not all([book_id, user_id, comment]):
+            return jsonify({'error': 'Missing required fields'}), 400
+
+        try:
+            conn = mysql.connector.connect(**db_config)
+            cursor = conn.cursor()
+
+            query = "INSERT INTO Comments (book_id, user_id, comment) VALUES (%s, %s, %s)"
+            cursor.execute(query, (book_id, user_id, comment))
+            conn.commit()
+
+            return jsonify({'message': 'Comment created successfully'}), 201
+
+        except mysql.connector.Error as err:
+            return jsonify({'error': 'Database error', 'message': str(err)})
+
+        finally:
+            if cursor:
+                cursor.close()
+            if conn and conn.is_connected():
+                conn.close()
 
 
 @app.route('/books/<int:book_id>/comments', methods=['GET'])
-def get_comments(book_id):
+def get_book_comments(book_id):
     conn = None
     cursor = None
 
@@ -82,13 +128,14 @@ def get_comments(book_id):
         conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor(dictionary=True)
 
-        cursor.execute("SELECT * FROM BookRatings WHERE book_id = %s", (book_id,))
+        query = "SELECT * FROM Comments WHERE book_id = %s"
+        cursor.execute(query, (book_id,))
         comments = cursor.fetchall()
 
         return jsonify(comments)
 
     except mysql.connector.Error as err:
-        return jsonify({'error': 'Database error', 'message': str(err)}), 500
+        return jsonify({'error': 'Database error', 'message': str(err)})
 
     finally:
         if cursor:
@@ -101,29 +148,26 @@ def get_comments(book_id):
 def get_average_rating(book_id):
     conn = None
     cursor = None
+
     try:
         conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor(dictionary=True)
-        cursor.execute("SELECT AVG(rating) AS rating FROM BookRatings WHERE book_id = %s", (book_id,))
+
+        query = "SELECT AVG(rating) AS average_rating FROM Ratings WHERE book_id = %s"
+        cursor.execute(query, (book_id,))
         avg_rating = cursor.fetchone()
 
-        # Ensure avg_rating is a dict before proceeding
-        if not isinstance(avg_rating, dict):
-            raise TypeError("avg_rating is not a dictionary.")
-
-        return jsonify({'average_rating': avg_rating['rating']})
+        return jsonify({'average_rating': avg_rating['average_rating']})
 
     except mysql.connector.Error as err:
-        return jsonify({'error': 'Database error', 'message': str(err)}), 500
-    except TypeError as te:
-        return jsonify({'error': 'Type error', 'message': str(te)}), 500
+        return jsonify({'error': 'Database error', 'message': str(err)})
+
     finally:
+        if cursor:
+            cursor.close()
         if conn and conn.is_connected():
-            if cursor:
-                cursor.close()
             conn.close()
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
